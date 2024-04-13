5c4fcb2303644b22f5b6481865f05a012e4415b0
Abdelrahman Elsawy <166948613+xqzvyb@users.noreply.github.com>
2024-04-13 22:22:25 +0300

    Update string.c

--- string.c
+++ string.c
@@ -1,11 +1,1 @@
-#include<stdio.h>
 
-void main()
-{
-	char name[]="Amgad";
-	
-	name[5]=='\0'? printf("%c \n", name[5]): printf("No \n");
-	fflush(stdout);
-	scanf("%s", name);
-	printf("%s", name);
-}
